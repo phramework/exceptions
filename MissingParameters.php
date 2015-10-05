020dfad38850aafa003b9e54ae25eb25fc034e75
Xenophon Spafaridis <nohponex@gmail.com>
2015-10-05 18:16:33 +0300

    Move src/Phramework to src/

---
+++ MissingParameters.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Phramework\Exceptions;
+
+/**
+ * MissingParamentersException
+ * Used to throw an \Exception, when there are some missing parameters.
+ * @license https://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
+ * @author Spafaridis Xenophon <nohponex@gmail.com>
+ */
+class MissingParameters extends \Exception
+{
+    //Array with the parameters
+    private $parameters;
+
+    /**
+     *
+     * @param array $parameters Array with the names of missing parameters
+     */
+    public function __construct($parameters)
+    {
+        parent::__construct('missing_parameters_exception', 400);
+        $this->parameters = $parameters;
+    }
+
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+}
